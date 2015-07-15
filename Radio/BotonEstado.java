b809a6ba1c9b9348b0f35f7b59440d3e6b71f216
JuanCarlosTapia <jc_tapia95@hotmail.com>
2015-07-15 15:43:44 -0600

    Update BotonEstado.java

--- Radio/BotonEstado.java
+++ Radio/BotonEstado.java
@@ -2,7 +2,7 @@
 public class BotonEstado implements IntBoton {
 
 	
-	public void onClick(Radio rad) {
+	public void onClick(Radio rad) { // Este metodo sirve para apagar el radio.
 		if (rad.getEstado()==true){
 			rad.setEstado(false);
 		}
