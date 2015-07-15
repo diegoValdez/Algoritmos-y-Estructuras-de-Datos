6a2f633438f6892c42ce86650c78800e5de6d7de
JuanCarlosTapia <jc_tapia95@hotmail.com>
2015-07-15 15:42:53 -0600

    Update BotonAmFm.java

--- Radio/BotonAmFm.java
+++ Radio/BotonAmFm.java
@@ -1,7 +1,7 @@
 
 public class BotonAmFm implements IntBoton {
 
-	public void onClick(Radio rad) {
+	public void onClick(Radio rad) { // Este metodo cambia de AM a FM y viceversa. Tambien cambia la frecuencia sintonizada.
 		if (rad.getEstacion().equals("AM")){
 			rad.setEstacion("FM");
 			rad.setFrecuencia(87.9);
